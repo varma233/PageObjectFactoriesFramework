418f336deeb4fa72754983b55e4279c4a9a80e1d
Naveen <naveenkakarlapudi@gmail.com>
2020-04-14 04:00:03 +0530

    changed browser

--- src/main/java/com/expedia/india/base/Constants.java
+++ src/main/java/com/expedia/india/base/Constants.java
@@ -2,7 +2,7 @@ package com.expedia.india.base;
 
 public class Constants { 
 
-	public static final String browser = "chrome";
+	public static final String browser = "firefox";
 	public static final String baseUrl = "https://www.expedia.co.in/"; 
 
 }
